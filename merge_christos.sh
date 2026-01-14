815f6af2720e0e02099f7e30a2d8b0e4eecd4725
Vector <ValikProgrammer@gmail.com>
2026-01-14 12:58:09 +0200

    .

---
+++ merge_christos.sh
@@ -0,0 +1,13 @@
+git stash 
+git switch master
+git pull master
+git switch christos
+git merge master
+git stash apply
+
+
+
+##
+# 
+#
+##
---
+++ valik_commit_master.sh
@@ -0,0 +1,8 @@
+# valik branch
+git add . ; git commit -m "."; git push origin master 
+
+git switch master
+# merge from valik to master 
+git merge -m "update" valik 
+
+git push 
